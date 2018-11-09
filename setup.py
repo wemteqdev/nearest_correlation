d849d61a3c885ee4e314e0a314d1653ab4430bdb
EC2 Default User <ec2-user@ip-172-31-12-156.us-east-2.compute.internal>
2018-11-09 05:32:39 +0000

    added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name="nearest_correlation",
+    packages=['nearest_correlation'],
+    version="1.1.0",
+    description="Python versions of nearest correlation matrix algorithms",
+    author='Eka',
+    author_email='cto@wemteq.com',
+    url='https://github.com/wemteqdev/nearest_correlation',
+    keywords=['algorithm'],
+    install_requires=['numpy>=1.11.3', 'scipy>=0.19.0'],
+    include_package_data=True,
+    entry_points={'console_scripts': ['nearest_correlation = nearest_correlation.__main__:main']},
+    zip_safe=False,
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    classifiers=[
+        'Development Status :: 1 - Beta'
+    ]
+)
